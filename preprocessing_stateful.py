dad6cb12ef7acc1be0356eff301aed2ce3b484dc
Sander <hanakmumakil23@gmail.com>
2018-11-18 11:14:06 +0100

    Adds more work, also a specialist

--- preprocessing_stateful.py
+++ preprocessing_stateful.py
@@ -26,12 +26,13 @@ dataset_names = prep.load_all_dataset_names(fs1_dirpath)
 datasets = [dataset[:, 1:] for dataset in datasets] # Remove the headers
 
 dataset_id_names = dict(zip(np.arange(len(dataset_names)), dataset_names))
-longest_song = max(datasets[i].shape[1] for i in range(len(datasets)))
+longest_song = max(datasets[i].shape[1] for i in range(len(datasets))) + 1 # account for the EOF marker
 sequence_length = 10 #40
 #b_size = 71 # 4047=3*19*71, 0.2 split ==> 3237 = 3*13*83
-number_of_batches = longest_song//sequence_length + 1
+num_batches = longest_song//sequence_length + 1
 num_keys = len(datasets[0])
-pad_length = number_of_batches*sequence_length
+pad_length = num_batches*sequence_length
+num_songs = len(datasets)
 
 def preprocess_for_stateful_with_padding(dataset, num_songs, sequence_length, num_batches, num_keys, pad_length):
     big_ass_ndarray = [[[] for a in range(num_songs)] for b in range(num_batches)]
@@ -41,11 +42,23 @@ def preprocess_for_stateful_with_padding(dataset, num_songs, sequence_length, nu
             big_ass_ndarray[i][j] = songs_padded[j, i*sequence_length:(i+1)*sequence_length]
     return np.array(big_ass_ndarray)
 
-
-
-
+def preprocess_for_3d(dataset, num_songs, sequence_length, num_keys):
+    big_af = [[] for song in range(num_songs)]
+    for i in range(num_songs):
+        for j in range(len(dataset[i])//sequence_length+1):
+            if j == len(dataset[i])//sequence_length:
+                last_part = datasets[i][-sequence_length:]
+                pads = []
+                final = datasets[i][-1:]
+                for k in range((j+1)*sequence_length-len(dataset[i])):
+                    pads.append([-1.0 for _ in range(num_keys)])
+                last_part = np.append(last_part, np.array(pads), axis=0)
+                big_af[i].append(last_part)
+            else:
+                big_af[i].append(dataset[i][j*sequence_length:(j+1)*sequence_length])
+    return np.array(big_af)
 datasets = np.array([dataset.T for dataset in datasets])
-xs = preprocess_for_stateful_with_padding(datasets, len(datasets), sequence_length, number_of_batches, num_keys, pad_length)
+xs = preprocess_for_stateful_with_padding(datasets, num_songs, sequence_length, num_batches, num_keys, pad_length)
 datasets_labels = np.array([np.append(dataset[1:,:], np.array([np.ones(num_keys)]), axis=0) for dataset in datasets])
-ys = preprocess_for_stateful_with_padding(datasets, len(datasets), sequence_length, number_of_batches, num_keys, pad_length)
+ys = preprocess_for_stateful_with_padding(datasets_labels, num_songs, sequence_length, num_batches, num_keys, pad_length)
 print("done")
